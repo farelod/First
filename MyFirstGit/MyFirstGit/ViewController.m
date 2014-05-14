3d876bd0a33100e69aeabb4ad84cb6b18cd2edd6
farelod <411082779@qq.com>
2014-05-14 10:23:21 +0800

    hehehehehehe

---
+++ MyFirstGit/MyFirstGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFirstGit
+//
+//  Created by ibokan on 14-5-14.
+//  Copyright (c) 2014年 ui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
